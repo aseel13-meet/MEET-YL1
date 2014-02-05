c3dc141ab511cf73f894b4d119e8484bb4fe9bb9
Aseel Bataha <aseel13@meet.mit.edu>
2014-02-05 19:51:50 +0200

    lab4

---
+++ Lab4/number.py
@@ -0,0 +1,17 @@
+class Integer(object):
+	def __init__ ( self, num ):
+		self.number= num
+		
+	def display():
+		print self.number
+
+class Test (Integer):
+	def __init__ (self , num):
+		super(Test , self). __init__(uni)
+		self.test =test
+	def signature (self):
+		return str(self.number) + ", " + self.test (self)
+	
+if __name__=="__main__":
+	print "Michele" 
+
